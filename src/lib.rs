050f5a6b71171459a6683559c58c32ba8ea99569
Sakars <o.putaans@gmail.com>
2024-06-16 22:37:33 +0300

    Added an additional test

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,31 @@ pub mod rule;
 
 #[cfg(test)]
 mod tests {
+    use crate::Parsable;
 
     #[test]
     fn parse_expression() {
         let identifier = seq!("identifier" =>
         sor!(
-            char!('_')
+            char!('_'),
+            ranges!(('a','z'), ('A','Z'))
+        ),
+        star!(
+            sor!(
+                char!('_'),
+                ranges!(('a','z'), ('A','Z'), ('0','9'))
+            )
         )
         );
+        let res = identifier.parse("a_1");
+        assert_eq!(res.is_some(), true);
+        let node = res.unwrap();
+        assert_eq!(node.content, "a_1");
+        assert_eq!(node.children.len(), 2);
+        assert_eq!(node.children[0].content, "a");
+        assert_eq!(node.children[1].content, "_1");
+        assert_eq!(node.children[1].children.len(), 2);
+        assert_eq!(node.children[1].children[0].content, "_");
+        assert_eq!(node.children[1].children[1].content, "1");
     }
 }
