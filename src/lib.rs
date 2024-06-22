3bee3875d653d4e47be8894185cfa697f40c1700
Sakars <o.putaans@gmail.com>
2024-06-22 20:36:27 +0300

    Removed use statements

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,7 @@ pub use filter::*;
 
 #[cfg(test)]
 mod tests {
-    use std::{fs::File, io::Write};
 
-    use crate::Filter;
     #[test]
     fn back_and_forth() {
         let a_name = String::from("ARule");
